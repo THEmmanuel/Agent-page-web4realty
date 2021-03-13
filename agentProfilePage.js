ce2eff982b00acc7f1bcb1befcfddde265fbfdd4
Emmanuel Ayodele <emmayodayo@gmail.com>
2021-03-13 13:27:24 +0100

    Added bbasic styling for property cards

--- agentProfilePage.js
+++ agentProfilePage.js
@@ -1,22 +1,35 @@
 import {
-    agentProfile, properties, 
+    agentProfile,
+    properties,
 } from "./mockAPI.js";
-
-const propertiesList = document.querySelector('.properties');
-const agentProfilePicture = document.querySelector('.agent-profle-picture');
-const agentName = document.querySelector('.agent-profile-name');
 
 
 const renderAgentProfile = () => {
+    const agentProfilePicture = document.querySelector('.agent-profle-picture');
+    const agentName = document.querySelector('.agent-profile-name');
+    const agentBio = document.querySelector('.agent-bio');
+    const agentRating = document.querySelector('.agent-rating');
+
     // agentProfilePicture.src = agentProfile.profile_picture;
     agentName.innerHTML = agentProfile.name;
-}  
+    agentRating.innerHTML = agentProfile.rating;
+    agentBio.innerHTML = agentProfile.bio;
+}
 
 const renderAgentProperties = () => {
+    const propertiesList = document.querySelector('.properties');
+
     const propertyRows = properties.map(property => {
         return `<li>
-            <div class = "propert-card">
-                name: ${property.name}
+            <div class = "property-card">
+            <img src="https://images.ratemyagent.com/ratemyagent/image/upload/q_auto:eco,f_auto,w_900,h_600,c_limit/cdn-usa/listing/329ec365-b4d3-43a2-83d0-d51624b72ac0.png" alt="">
+
+            <span>${property.price}</span>
+            <span>${property.address}</span>
+            <span>${property.status}</span>
+            <span>solld by ...</span>
+            
+    
             </div>
         </li>`
     })
--- mockAPI.js
+++ mockAPI.js
@@ -18,11 +18,115 @@ export const agentProfile = {
 //Property data.
 export const properties = [{
         id: '01',
-        name: '1975 Blooming Hills Dr, Prescott, AZ, 86301',
+        address: '1975 Blooming Hills Dr, Prescott,AZ, 86301',
         price: '269,000',
         bedroom_number: '3',
         bathroom_number: '2',
-        status: 'sold'
+        status: 'sold',
+        type: 'condo',
+        carspace_number: '2',
+    },
+
+    {
+        id: '02',
+        name: '1975 Blooming Hills Dr, Prescott, AZ, 86301',
+        price: '269,000',
+        bedroom_number: '3',
+        bathroom_number: '2'
+    },
+
+    {
+        id: '03',
+        name: '1975 Blooming Hills Dr, Prescott, AZ, 86301',
+        price: '269,000',
+        bedroom_number: '3',
+        bathroom_number: '2'
+    },
+
+    {
+        id: '04',
+        name: '1975 Blooming Hills Dr, Prescott, AZ, 86301',
+        price: '269,000',
+        bedroom_number: '3',
+        bathroom_number: '2'
+    },
+    {
+        id: '01',
+        address: '1975 Blooming Hills Dr, Prescott,AZ, 86301',
+        price: '269,000',
+        bedroom_number: '3',
+        bathroom_number: '2',
+        status: 'sold',
+        type: 'condo',
+        carspace_number: '2',
+    },
+
+    {
+        id: '02',
+        name: '1975 Blooming Hills Dr, Prescott, AZ, 86301',
+        price: '269,000',
+        bedroom_number: '3',
+        bathroom_number: '2'
+    },
+
+    {
+        id: '03',
+        name: '1975 Blooming Hills Dr, Prescott, AZ, 86301',
+        price: '269,000',
+        bedroom_number: '3',
+        bathroom_number: '2'
+    },
+
+    {
+        id: '04',
+        name: '1975 Blooming Hills Dr, Prescott, AZ, 86301',
+        price: '269,000',
+        bedroom_number: '3',
+        bathroom_number: '2'
+    },
+    {
+        id: '01',
+        address: '1975 Blooming Hills Dr, Prescott,AZ, 86301',
+        price: '269,000',
+        bedroom_number: '3',
+        bathroom_number: '2',
+        status: 'sold',
+        type: 'condo',
+        carspace_number: '2',
+    },
+
+    {
+        id: '02',
+        name: '1975 Blooming Hills Dr, Prescott, AZ, 86301',
+        price: '269,000',
+        bedroom_number: '3',
+        bathroom_number: '2'
+    },
+
+    {
+        id: '03',
+        name: '1975 Blooming Hills Dr, Prescott, AZ, 86301',
+        price: '269,000',
+        bedroom_number: '3',
+        bathroom_number: '2'
+    },
+
+    {
+        id: '04',
+        name: '1975 Blooming Hills Dr, Prescott, AZ, 86301',
+        price: '269,000',
+        bedroom_number: '3',
+        bathroom_number: '2'
+    },
+    {
+        id: '01',
+        address: '1975 Blooming Hills Dr, Prescott,AZ, 86301',
+        price: '269,000',
+        bedroom_number: '3',
+        bathroom_number: '2',
+        status: 'sold',
+        type: 'condo',
+        carspace_number: '2',
     },
 
     {
