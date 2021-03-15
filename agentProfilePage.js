ed52e66c2aecde3d9d8bbbd5fafab36429805582
Emmanuel Ayodele <emmayodayo@gmail.com>
2021-03-15 08:48:34 +0100

    Added styling to agent profile

--- agentProfilePage.js
+++ agentProfilePage.js
@@ -41,7 +41,7 @@ const renderAgentProperties = () => {
         <div>
         <span class="property-status">${property.status}</span>
         <img class="property-image"
-            src="https://images.ratemyagent.com/ratemyagent/image/upload/q_auto:eco,f_auto,w_900,h_600,c_limit/cdn-usa/listing/329ec365-b4d3-43a2-83d0-d51624b72ac0.png">
+            src=${property.property_image}>
         </div>
     
             <div class="property-information-wrapper">
@@ -52,7 +52,6 @@ const renderAgentProperties = () => {
                 </div>
     
                 <button class="property-share-btn">
-                    share
                 </button>
 
             </div>
@@ -61,19 +60,18 @@ const renderAgentProperties = () => {
 
             <div class="property-icons-wrapper">
             <div>
-            <img src="https://img.icons8.com/android/16/000000/bedroom.png"
-                class="property-information-icon" />
+            <img src="https://img.icons8.com/ios/16/808080/bedroom.png"              class="property-information-icon" />
             <span>2</span>
         </div>
 
         <div>
-            <img src="https://img.icons8.com/android/16/000000/bedroom.png"
-                class="property-information-icon" />
+
+        <img src="https://img.icons8.com/ios/16/808080/toilet-bowl.png"                class="property-information-icon" />
             <span>2</span>
         </div>
 
         <div>
-            <img src="https://img.icons8.com/android/16/000000/bedroom.png"
+        <img src="https://img.icons8.com/ios/16/808080/garage.png"
                 class="property-information-icon" />
             <span>2</span>
         </div>
